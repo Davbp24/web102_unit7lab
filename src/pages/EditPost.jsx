9108d6aa3700416d6ebaee1c61b76913cc66752e
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2025-11-05 21:59:39 -0500

    pushed

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,42 +1,89 @@
-import {useState} from 'react'
-import { useParams } from 'react-router-dom'
-import './EditPost.css'
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import "./EditPost.css";
+import { supabase } from "../client";
 
-const EditPost = ({data}) => {
+const EditPost = ({ data }) => {
+  const { id } = useParams();
+  const [post, setPost] = useState({
+    id: null,
+    title: "",
+    author: "",
+    description: "",
+  });
 
-    const {id} = useParams()
-    const [post, setPost] = useState({id: null, title: "", author: "", description: ""})
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
-    const handleChange = (event) => {
-        const {name, value} = event.target
-        setPost( (prev) => {
-            return {
-                ...prev,
-                [name]:value,
-            }
-        })
-    }
+  const deletePost = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div>
-            <form>
-                <label htmlFor="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} onChange={handleChange} /><br />
-                <br/>
+    await supabase.from("Posts").delete().eq("id", id);
+    window.location = '/';
+  };
 
-                <label htmlFor="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} onChange={handleChange} /><br />
-                <br/>
+  const updatePost = async (event) => {
+    event.preventDefault();
 
-                <label htmlFor="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange} >
-                </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
-            </form>
-        </div>
-    )
-}
+    await supabase
+      .from("Posts")
+      .update({
+        title: post.title,
+        author: post.author,
+        description: post.description,
+      })
+      .eq("id", id);
 
-export default EditPost
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">Title</label> <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={post.title}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="author">Author</label>
+        <br />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={post.author}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="description">Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          value={post.description}
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" onClick={updatePost} />
+        <button className="deleteButton" onClick={deletePost}>Delete</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditPost;
